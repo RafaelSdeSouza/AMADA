6195efc4b05dc0e83edf5ba10a1d09a47aed84ec
Rafael <rafael.2706@gmail.com>
2014-11-07 10:00:50 +0100

    add options to graph

--- R/plotgraph.R
+++ R/plotgraph.R
@@ -31,7 +31,8 @@
 #' @keywords misc
 #' @export 
  
-plotgraph<-function(corr){
+plotgraph<-function(corr,layout=c("spring","circular")){
+  layout <- match.arg(layout)
   
   rownames(corr)<-colnames(corr)
   dissimilarity<-1-abs(corr)
@@ -59,7 +60,7 @@ plotgraph<-function(corr){
   # Color scheme
   col_q<-colorRampPalette(c("#e41a1c","#377eb8","#4daf4a","#984ea3","#ff7f00","#ffff33","#a65628","#f781bf"))
   
-  qgraph(M,layout="spring",type="factorial",legend=FALSE,vsize=7.75,groups=Biggroup,
+  qgraph(M,layout=layout,type="factorial",legend=FALSE,vsize=7.75,groups=Biggroup,
          label.scale=F,label.cex=0.65,edge.color = "black",labels=rownames(corr),
          transparency = F,bg=F,diag=F,color=col_q(nc),mar=c(2,2,2,2),vTrans=235,arrows=T)
   
